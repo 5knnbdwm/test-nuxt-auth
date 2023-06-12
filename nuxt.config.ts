804c3b7096149ee6620aeaec50a39dbd9f052bf7
5knnbdwm <samuel@soeller.dev>
2023-06-12 18:33:24 +0200

    remove stuff

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,8 +5,6 @@ import { createResolver } from "@nuxt/kit";
 const { resolve } = createResolver(import.meta.url);
 
 export default defineNuxtConfig({
-  ssr: false,
-  css: ["@fortawesome/fontawesome-free/css/all.min.css"],
   modules: ["@nuxtjs/tailwindcss", "@sidebase/nuxt-auth"],
   auth: {
     origin: process.env.ORIGIN,
